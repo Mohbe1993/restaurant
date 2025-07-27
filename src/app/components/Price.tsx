82cf593c856aebbcdd669787f2b2621972409fde
mohbe1993 <mohamedbehairy1993@yahoo.com>
2025-07-27 05:07:12 +0400

    products

---
+++ src/app/components/Price.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+
+type props = {
+
+    price: number,
+    id: number,
+    options: [
+        {
+            title: "Small",
+            additionalPrice: 0,
+        },
+        {
+            title: "Medium",
+            additionalPrice: 4,
+        },
+        {
+            title: "Large",
+            additionalPrice: 6,
+        },
+    ],
+
+}
+
+function Price({ price,
+    id,
+    options }: props) {
+
+
+    const [total, setTotal] = useState(price)
+    const [quantity, setQuantity] = useState(1)
+    const [selected, setSelected] = useState(0)
+
+    useEffect(() => {
+        setTotal(quantity * (options ? price + options[selected].additionalPrice : price))
+    }, [options, price, quantity, selected])
+    return (
+        <div className='flex flex-col gap-4'>
+            <h2 className='text-2xl font-bold'>$ {total.toFixed(2)}</h2>
+            <div className='flex gap-4'>
+                {options.map((option, index) => (
+                    <button key={option.title} className='p-2 ring-1 ring-red-500 rounded-md ' style={{ background: selected === index ? 'red' : 'white', color: selected === index ? 'white' : 'red' }} onClick={() => setSelected(index)}>{option.title}</button>
+                ))}
+            </div>
+            <div className=' flex justify-between items-center gap-2'>
+                <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
+                    <span>Quantity</span>
+                    <div className='flex gap-2 items-center'>
+                        <button onClick={() => setQuantity(prev => (prev > 1 ? prev - 1 : 1))}>{'<'}</button>
+                        <span>{quantity}</span>
+                        <button onClick={() => setQuantity(prev => (prev < 9 ? prev + 1 : 9))}>{'>'}</button>
+                    </div>
+                </div>
+                <button className='p-2 ring-1 w-1/2 ring-red-500 rounded-md'>Add to Cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Price
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,20 @@
+import { singleProduct } from '@/app/components/data'
+import Price from '@/app/components/Price'
+import Image from 'next/image'
 import React from 'react'
 
 function SingleProduct() {
     return (
-        <div>Products</div>
+        <div className='p-4 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center'>
+            {singleProduct.img && <div className='relative w-full h-1/2 md:h-[70%]'>
+                <Image src={singleProduct.img} alt='' className='object-contain' fill />
+            </div>}
+            <div className='h-1/2 flex flex-col gap-4  md:h-[70%] md:justify-center'>
+                <h1 className='text-3xl font-bold'>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+                <Price price={singleProduct.price} id={singleProduct.id} options ={singleProduct.options} />
+            </div>
+        </div>
     )
 }
 
